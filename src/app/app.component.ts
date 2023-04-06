82300677ba5f43a164c81d405fd978482c532a6f
Ruwaid imran <ruwaidmran@gmail.com>
2023-04-06 15:30:26 +0500

    create component with CLI nesting Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'this my-first-app';
-  name = 'Ruwaid';
+   title = 'this my-first-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { httModule } from '@angular/htt';
 import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, ServersComponent],
+  imports: [BrowserModule, AppRoutingModule, /*FormsModule, httpModule*/],
   providers: [],
   bootstrap: [AppComponent],
 })
