7c1b25f458ef03b30ca4faba7204a378946a9afe
Ruwaid imran <ruwaidmran@gmail.com>
2023-04-07 15:28:14 +0500

    create multiple component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { httModule } from '@angular/htt';
+// import { httModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { ServersComponent } from './servers/servers.component';
-
-
+import { StudentsComponent } from './students/students.component';
+import { SecondayTeacherComponent } from './seconday-teacher/seconday-teacher.component';
+import { SupportComponent } from './support/support.component';
 @NgModule({
-  declarations: [AppComponent, ServersComponent],
-  imports: [BrowserModule, AppRoutingModule, /*FormsModule, httpModule*/],
+  declarations: [AppComponent, ServersComponent, StudentsComponent, SecondayTeacherComponent, SupportComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule /*httpModule*/],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/seconday-teacher/seconday-teacher.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seconday-teacher',
+  templateUrl: './seconday-teacher.component.html',
+  styleUrls: ['./seconday-teacher.component.css']
+})
+export class SecondayTeacherComponent {
+
+}
