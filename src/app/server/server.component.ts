ee019922e262e01723a55359b98f186bf8951367
Ruwaid imran <ruwaidmran@gmail.com>
2023-04-20 13:11:43 +0500

    data binding and propert binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,10 +4,16 @@ import { NgModel } from '@angular/forms';
 @Component({
 selector : 'app-server',
 templateUrl: './server.component.html',
-// styleUrls: ['./server.component.css'],
+styleUrls: ['./server.component.css'],
 })
 export class ServerComponent{
-    title= "server Component"
+    title= "start server Component"
     name="John"
+    serverId : number = 20;
+    serverStatus: string = 'components status';
+    
+    getServerStatus(){
+        return this.serverStatus;
+    }
   
 }
